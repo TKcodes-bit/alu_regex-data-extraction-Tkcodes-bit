c39beaed201999c00049e2a79e7c5a123d06a803
TKcodes-bit <t.odongo@alustudent.com>
2025-05-19 18:15:42 +0300

    Impoer re and define regex for each data type

---
+++ datamine.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#Import re
+
+import re
+
+class datamine:
+    def__init__(self):
+        self.regex_rubric = {
+            "Email addressess" :  r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b',
+            "URLs" : r'(?i)\b((?:https?://|www\.)[^\s]+)',
+            "Phone numbers" : r'\b\d{3}[-.\s]??\d{3}[-.\s]??\d{4}\b',
+            "Credit card numbers" : r'\b(?:\d{4}[-.\s]?){3}\d{4}\b',
+            "Time" : r'\b(?:[01]\d|2[0-3]):[0-5]\d\b',
+            "HTML tags" : r'<[^>]+>',
+            "Hash tags" : r'#\w+',
+            "Currency amounts" : r'\$\d+(?:\.\d{2})?',
+        }
